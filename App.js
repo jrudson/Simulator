a23ade035376c2a4f98f0385f5b5e54cce9ceabe
jrudson <jonerudsonc@gmail.com>
2022-09-07 19:59:11 -0300

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Image, Button, TextInput } from 'react-native';
+import nations from './nations';
 
 export default function App() {
+
+  const [nation, setNation] = useState(nations);
+  const [searchValue, setSearchValue] = useState(Number);
+  const [nationPoints, setNationPoints] = useState(0);
+
+  function Games() {
+    return (
+      <Text>
+        {nation[0].name} 
+      <TextInput
+        style={styles.input} 
+        value={searchValue}
+        onChange={(value) => setSearchValue(value)}
+      /> 
+       x  
+      <TextInput 
+        style={{borderColor: 'black'}}
+      /> 
+        {nation[1].name}
+      </Text>
+    )
+  }
+
+  function alterar() {
+    setNationPoints(pontos => {
+      return pontos + 1
+    });
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <Text> {item.name} {nationPoints} </Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={nation}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      <View>
+        <Games />
+      </View>
+      <Button 
+        onPress={() => {
+          alterar()
+        }}
+        title='altere a pontuação'
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +66,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 30
+  },
+
+  input: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#363636',
+    margin: 30,
+    borderRadius: 5,
+    fontSize: 19,
+    paddingLeft: 15,
+    paddingRight: 15,
+    color: '#FFFFFF',
   },
 });
+ 
---
+++ nations.js
@@ -0,0 +1,23 @@
+const nations = [
+    {
+        id: 1,
+        name: 'Qatar',
+    },
+
+    {
+        id: 2,
+        name: 'Ecuador',
+    },
+
+    {
+        id: 3,
+        name: 'Senegal',
+    },
+
+    {
+        id: 4,
+        name: 'Netherlands',
+    }
+];
+
+export default nations;
