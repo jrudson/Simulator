cac6fb77dd10493b1d6c5d83b38e1662cb989bb4
jrudson <jonerudsonc@gmail.com>
2022-09-10 00:17:51 -0300

    pontuações conversando com inputs

--- App.js
+++ App.js
@@ -1,41 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, FlatList, Image, Button, TextInput } from 'react-native';
-import nations from './nations';
+// import nations from './nations';
 
 export default function App() {
 
-  const [nation, setNation] = useState(nations);
-  const [searchValue, setSearchValue] = useState(Number);
-  const [nationPoints, setNationPoints] = useState(0);
+  // const [nation, setNation] = useState(nations);
+  const [time1, setTime1] = useState({"name": '', "points": '', "gols": ''});
+  const [time2, setTime2] = useState({"name": '', "points": '', "gols": ''});
+  const [input1, setInput1] = useState();
+  const [input2, setInput2] = useState();
 
   function Games() {
     return (
-      <Text>
-        {nation[0].name} 
-      <TextInput
-        style={styles.input} 
-        value={searchValue}
-        onChange={(value) => setSearchValue(value)}
-      /> 
-       x  
-      <TextInput 
-        style={{borderColor: 'black'}}
-      /> 
-        {nation[1].name}
-      </Text>
+      <View>
+        <View style={styles.viewPlacar}>
+          <Text>
+            Qatar
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setInput1}
+            value={input1}
+            keyboardType='numeric'
+            textAlign='center'
+            maxLength={2}
+          /> 
+          <Text> x </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setInput2}
+            value={input2}
+            keyboardType='numeric'
+            textAlign='center'
+            maxLength={2}
+          />
+          <Text>
+            Ecuador
+          </Text>
+        </View>
+      </View>
     )
   }
 
   function alterar() {
-    setNationPoints(pontos => {
-      return pontos + 1
-    });
+    if(input1 !== '' && input2 !== '') {
+      if(input1 > input2) {
+        setTime1({"name": "Qatar", "gols": input1,  "points": 3});
+        setTime2({"name": "Ecuador", "gols": input2, "points": 0});
+      } else if(input2 > input1) {
+        setTime1({"name": "Qatar", "gols": input1,  "points": 0});
+        setTime2({"name": "Ecuador", "gols": input2, "points": 3});
+      } else {
+        setTime1({"name": "Qatar", "gols": input1,  "points": 1});
+        setTime2({"name": "Ecuador", "gols": input2, "points": 1});
+      }
+    }
   };
+  
+  const nation = [time1, time2];
+
 
   const renderItem = ({ item }) => {
     return (
       <View>
-        <Text> {item.name} {nationPoints} </Text>
+        <Text> {item.name} {item.points} </Text>
       </View>
     );
   };
@@ -45,12 +73,12 @@ export default function App() {
       <FlatList
         data={nation}
         renderItem={renderItem}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id}
       />
       <View>
         <Games />
       </View>
-      <Button 
+      <Button
         onPress={() => {
           alterar()
         }}
@@ -62,7 +90,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
@@ -70,15 +98,15 @@ const styles = StyleSheet.create({
   },
 
   input: {
-    flex: 1,
-    height: 50,
-    backgroundColor: '#363636',
-    margin: 30,
-    borderRadius: 5,
-    fontSize: 19,
-    paddingLeft: 15,
-    paddingRight: 15,
-    color: '#FFFFFF',
+    height: 20,
+    margin: 5,
+    borderWidth: 1,
+    padding: 2
   },
+
+  viewPlacar: {
+    flexDirection: 'row',
+  }
 });
- 
+
+// https://www.youtube.com/watch?v=UUY3cYUif0Y
